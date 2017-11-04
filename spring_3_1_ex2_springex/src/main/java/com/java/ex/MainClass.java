ac197b8b6d066be1cc89f0dbb574edaa547ed8db
Namjun Kim <namjunemy@gmail.com>
2017-11-05 02:21:49 +0900

    spring 설정 파일 추가한 예제

---
+++ spring_3_1_ex2_springex/src/main/java/com/java/ex/MainClass.java
@@ -0,0 +1,17 @@
+package com.java.ex;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+  public static void main(String[] args) {
+    String configLocation = "classpath:applicationCTX.xml";
+    AbstractApplicationContext ctx = new GenericXmlApplicationContext(configLocation);
+    MyCalculator myCalculator = ctx.getBean("myCalculator", MyCalculator.class);
+
+    myCalculator.add();
+    myCalculator.sub();
+    myCalculator.mul();
+    myCalculator.div();
+  }
+}
