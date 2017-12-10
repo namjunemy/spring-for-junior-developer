52e6b1cc4f5e9def47514f9add9792b160a4f717
Namjun Kim <namjunemy@gmail.com>
2017-12-10 22:11:35 +0900

    list출력 메소드 mybatis적용

--- spring_28_1_ex1_springex/src/main/java/com/javalec/ex/HomeController.java
+++ spring_28_1_ex1_springex/src/main/java/com/javalec/ex/HomeController.java
@@ -7,6 +7,7 @@ import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.javalec.mybatis.dao.ContentDao;
+import com.javalec.mybatis.dao.IDao;
 import com.javalec.mybatis.dto.ContentDto;
 
 @Controller
@@ -23,6 +25,9 @@ public class HomeController {
   private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
   ContentDao dao;
+
+  @Autowired
+  private SqlSession sqlSession;
 
   @Autowired
   public void setDao(ContentDao dao) {
@@ -45,8 +50,8 @@ public class HomeController {
 
   @RequestMapping("/list")
   public String list(Model model) {
-    ArrayList<ContentDto> dtos = dao.listDao();
-    model.addAttribute("list", dtos);
+    IDao dao = sqlSession.getMapper(IDao.class);
+    model.addAttribute("list", dao.listDao());
 
     return "/list";
   }
