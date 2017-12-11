c657ecd17ba30c159ad13f704c84cb202bfb569a
Namjun Kim <namjunemy@gmail.com>
2017-12-11 23:39:46 +0900

    modify controller

--- spring_28_1_ex1_springex/src/main/java/com/javalec/ex/HomeController.java
+++ spring_28_1_ex1_springex/src/main/java/com/javalec/ex/HomeController.java
@@ -15,7 +15,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.javalec.mybatis.dao.ContentDao;
 import com.javalec.mybatis.dao.IDao;
 
 @Controller
